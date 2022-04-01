5ef4be48e97938b9aef0902d35b4e81c8cfcf3ab
Artem Kurov <kur0v-artem@yandex.ru>
2022-04-01 21:28:35 +0300

    add search

--- src/pages/PhonePage/PhoneBook/PhoneBook.jsx
+++ src/pages/PhonePage/PhoneBook/PhoneBook.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import styles from './PhoneBook.module.scss';
 import { PhoneCard } from './PhoneCard/PhoneCard';
 import { useSelector } from 'react-redux';
-import { listPhone } from '../../../redux/selector';
+import { filteredPhone, listPhone } from '../../../redux/selector';
 
 export const PhoneBook = () => {
   const phones = useSelector(listPhone);
+  const filteredPhones = useSelector(filteredPhone);
   return (
     <div className={styles.phoneBook}>
       {phones.length ? (
         <ul className={styles.wrapperPhoneBook}>
-          {phones.map((item) => (
+          {filteredPhones.map((item) => (
             <PhoneCard key={item.id} {...item} />
           ))}
         </ul>
--- src/redux/selector.js
+++ src/redux/selector.js
@@ -5,4 +5,12 @@ const selectDomain = (state) => state.phones || initialState;
 
 export const listPhone = (state) => selectDomain(state).phones;
 
+export const inputValue = (state) => selectDomain(state).value;
+
 export const formStore = (state) => selectDomain(state).formValues;
+
+export const filteredPhone = createSelector([listPhone, inputValue], (phones, value) => {
+  return phones.filter((phone) => {
+    return phone.nameUser.toLowerCase().includes(value.toLowerCase());
+  });
+});
